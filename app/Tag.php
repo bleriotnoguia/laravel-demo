bc5c55717abe231ad7b59488ab5a5f2f39316b00
bleriot noguia <nstevebleriot@yahoo.fr>
2019-03-22 23:44:08 +0100

    bug fixed for multi select

--- app/Tag.php
+++ app/Tag.php
@@ -11,6 +11,6 @@ class Tag extends Model
 
     public function posts()
     {
-        return $this->belongsToMany('App\Post');
+        return $this->belongsToMany('App\Post', 'post_tag', 'tag_id', 'post_id');
     }
 }
